0f639b6f6dbfa82550f0f0a2df85cacbebb24797
Staylix <gregoire.gentil@orange.fr>
2017-11-28 09:38:45 +0100

    Moving on INSAturne

---
+++ app/src/main/java/com/gentil/gregoire/insaturne/model/User.java
@@ -0,0 +1,19 @@
+package com.gentil.gregoire.insaturne.model;
+
+import java.lang.reflect.GenericArrayType;
+
+/**
+ * Created by Gregoire on 13/11/2017.
+ */
+
+public class User {
+    private String mFirstName;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+}
